a0d1a2fd70fe8f663603b2bc8a9267416f46dac7
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-08-25 21:58:11 -0400

    fix-zeros-and-refactor: replace model from import tasks ts.

--- src/tasks/domain/tasks/tasks/transaction-scripts/import-tasks/import-tasks.transaction-script.ts
+++ src/tasks/domain/tasks/tasks/transaction-scripts/import-tasks/import-tasks.transaction-script.ts
@@ -1,17 +1,13 @@
 import { Injectable } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
-import { Task, TaskDocument } from "src/tasks/infrastructure/schema/task/task.schema";
+import { TaskRepository } from "src/tasks/infrastructure/repositories/task-repository";
+import { Task } from "src/tasks/infrastructure/schema/task/task.schema";
 
 @Injectable()
 export class ImportTasksTS {
 
-    constructor(@InjectModel(Task.name) private model: Model<TaskDocument>) { }
+    constructor(private readonly taskRepository: TaskRepository) { }
 
     async apply(tasks: Task[]) {
-        tasks.forEach(async task => {
-            const tempTask = await this.model.create(task);
-            tempTask.save();
-        })
+        return this.taskRepository.createTasks(tasks);
     }
 }
