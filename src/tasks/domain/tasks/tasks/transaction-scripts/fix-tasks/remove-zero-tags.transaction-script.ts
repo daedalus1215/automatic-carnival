2fd872ebf5e336ca085d841794154b9f0e00dd1c
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-08-25 21:57:57 -0400

    fix-zeros-and-refactor: add remove zero tag ts

---
+++ src/tasks/domain/tasks/tasks/transaction-scripts/fix-tasks/remove-zero-tags.transaction-script.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@nestjs/common";
+import { TaskRepository } from "src/tasks/infrastructure/repositories/task-repository";
+import { Task } from "src/tasks/infrastructure/schema/task/task.schema";
+
+@Injectable()
+export class RemoveZeroTags {
+    constructor(private readonly taskRepository: TaskRepository) { }
+
+    async apply() {
+        const tasksWithZeroTags = await this.taskRepository.findAllTasksWithZeroValuedTags();
+        return tasksWithZeroTags.map(async (task) => {
+            const updatedTask = await this.taskRepository.create({ ...task, tags: task.tags.filter(tag => tag !== '0') });
+            return updatedTask;
+          });
+    }
+}
